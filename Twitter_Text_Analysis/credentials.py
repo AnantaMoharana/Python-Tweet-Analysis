6dc9c1d9e40a38bd92c912be46c7fe573491d852
suryanshpsingh <67669995+suryanshpsingh@users.noreply.github.com>
2020-09-03 12:06:27 -0400

    Update credentials

--- Twitter_Text_Analysis/credentials.py
+++ Twitter_Text_Analysis/credentials.py
@@ -1,5 +1,5 @@
 # Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = '1295816426591223808-aQNJfuFYbO7IqJQExgvF7XcjRgcxMA'
-ACCESS_TOKEN_SECRET = 'N8Duu15JBWQtbieQKHuwRe9pmcoo2vSvWsXNJbTUA2lkE'
-CONSUMER_KEY = 'UlnkzbeRfw8dLYKiHPPaLuLC4'
-CONSUMER_SECRET = 'sh3ukgrS2135dwPCOu6UmL0pxwEaOrDnr8SKJxWwfcLnggzhk4'
+ACCESS_TOKEN = '----'
+ACCESS_TOKEN_SECRET = '----'
+CONSUMER_KEY = '----'
+CONSUMER_SECRET = '----'
